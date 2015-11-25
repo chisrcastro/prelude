ffbcdb793c67f44d77f9cc5307ddbdaa0938b1cf
Shelah Castro <shelah.castro@gmail.com>
2015-11-25 14:11:50 +0800

    [hinata] Initial config files on hinata machine.

---
+++ personal/text.el
@@ -0,0 +1,2 @@
+(setq-default truncate-lines 1)
+(setq prelude-clean-whitespace-on-save nil) ;https://github.com/bbatsov/prelude#disabling-whitespace-mode
---
+++ personal/themes.el
@@ -0,0 +1,14 @@
+;(add-to-list 'custom-theme-load-path "~/.emacs.d/personal/themes/ample")
+;(load-theme 'ample-light t)
+;
+
+;(load-theme 'anti-zenburn t)
+;
+
+;(load-theme 'wilson t)
+;
+
+;(load-theme 'jedit-grey t)
+;
+
+(disable-theme 'zenburn)
